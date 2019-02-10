f20be1a34ce32587426647e9b046741258e2c5a0
TEKITER-PC <tekiter8@gmail.com>
2019-02-10 21:53:49 +0900

    Create PyEngine class
    Basic Python interpreter engine class is created.

---
+++ Interactipy/Interactipy/Engine/PyEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactipy.Engine
+{
+    abstract class PyEngine
+    {
+
+        public StreamReader OutputStream { get; set; }
+        public StreamReader InformationStream { get; set; }
+        
+
+
+        public abstract bool Execute(string command);
+        public abstract void Start(string startupDirectory);
+        public abstract void Stop();
+
+        
+
+        
+    }
+}
---
+++ Interactipy/Interactipy/Engine/PyExeEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactipy.Engine
+{
+    class PyExeEngine : PyEngine
+    {
+        public string InterpreterPath { get; set; }
+
+        
+
+        private Process mainproc;
+
+        public PyExeEngine(string interpreterPath)
+        {
+            InterpreterPath = interpreterPath;
+        }
+
+        public override void Start(string startupDirectory)
+        {
+            mainproc = new Process();
+
+            ProcessStartInfo startinfo = new ProcessStartInfo(InterpreterPath, "-i -u");
+            startinfo.CreateNoWindow = true;
+            startinfo.RedirectStandardInput = true;
+            startinfo.RedirectStandardOutput = true;
+            startinfo.RedirectStandardError = true;
+            startinfo.UseShellExecute = false;
+
+            mainproc.StartInfo = startinfo;
+
+            
+
+            mainproc.Start();
+
+            OutputStream = mainproc.StandardOutput;
+            InformationStream = mainproc.StandardError;
+        }
+
+        public override void Stop()
+        {
+            if (!mainproc.HasExited)
+                mainproc.Kill();
+        }
+
+        public override bool Execute(string command)
+        {
+            mainproc.StandardInput.WriteLine(command);
+
+            return true;
+        }
+        
+    }
+}
