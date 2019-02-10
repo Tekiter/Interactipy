21897bdfef0688c443fa09feb9293d32abab11e6
TEKITER-PC <tekiter8@gmail.com>
2019-02-10 21:54:43 +0900

    Create basic implementation of PyEngine

--- Interactipy/Interactipy/MainWindow.xaml.cs
+++ Interactipy/Interactipy/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+using Interactipy.Engine;
 
 namespace Interactipy
 {
@@ -23,6 +26,70 @@ namespace Interactipy
         public MainWindow()
         {
             InitializeComponent();
+
+            
+        }
+
+        Thread readOutputThread;
+        Thread readinformationthread;
+        PyEngine engine;
+
+        private void txt_input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                //txt_output.Text += txt_input.Text + "\n";
+
+                txt_output.AppendText(txt_input.Text + "\n");
+                engine.Execute(txt_input.Text);
+                
+                txt_input.Clear();
+            }
+        }
+
+        private void readOutputLoop()
+        {
+            while (true)
+            {
+                char ch = (char)engine.OutputStream.Read();
+                DispatcherOperation op = Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    this.txt_output.AppendText(ch.ToString());
+                }));
+            }
+        }
+
+        private void readInformationLoop()
+        {
+            while (true)
+            {
+                char ch = (char)engine.InformationStream.Read();
+                DispatcherOperation op = Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    this.txt_output.AppendText(ch.ToString());
+                }));
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            engine = new PyExeEngine(@"C:\Users\geon0\AppData\Local\Programs\Python\Python37-32\python.exe");
+            engine.Start(@"C:\Users\geon0\AppData\Local\Programs\Python\Python37-32");
+
+            readOutputThread = new Thread(new ThreadStart(readOutputLoop));
+            readinformationthread = new Thread(new ThreadStart(readInformationLoop));
+            readinformationthread.Start();
+            readOutputThread.Start();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            engine.Stop();
+        }
+
+        private void txt_output_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
