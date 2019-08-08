8303fdf54b1ba552e286e3d5bb27683aeffc47d9
TEKITER-PC <tekiter8@gmail.com>
2019-08-08 20:16:46 +0900

    Renew project

---
+++ Interactipy/Interactipy/Engine/PyExecutable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactipy.Engine
+{
+    public class PyExecutable
+    {
+        public string Path { get; set; }
+
+
+        public PyExecutable(string path)
+        {
+            Path = path;
+        }
+    }
+}
