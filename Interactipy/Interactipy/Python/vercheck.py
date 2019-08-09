cd7883f41eccad988f3ac23fee4433e433a8e9e7
TEKITER-PC <tekiter8@gmail.com>
2019-08-09 16:11:27 +0900

    Update vercheck.py

--- Interactipy/Interactipy/Python/vercheck.py
+++ Interactipy/Interactipy/Python/vercheck.py
@@ -2,4 +2,4 @@
 import platform
 ver = sys.version_info
 bits = platform.architecture()[0]
-sys.stdout.write("Python %s %d.%d.%d\n" % (bits, ver[0], ver[1], ver[2]))
+sys.stdout.write("Python %d.%d.%d %s\n" % (ver[0], ver[1], ver[2], bits))
